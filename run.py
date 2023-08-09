3c082deeda6d0fad17dcbc4bb24acc446dbcd8f1
Marty Penner <martypenner@users.noreply.github.com>
2023-08-09 18:18:05 -0400

    refactor: clean up into smaller functions

--- run.py
+++ run.py
@@ -6,49 +6,65 @@ import requests
 from dotenv import load_dotenv
 from pydub import AudioSegment
 
-load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-podcast_url = ''
-if not os.path.exists('podcast.mp3'):
-  print('getting podcast')
-  response = requests.get(podcast_url)
+def download_podcast(podcast_url):
+    if not os.path.exists('podcast.mp3'):
+        print('Getting podcast')
+        response = requests.get(podcast_url)
 
-  with open('podcast.mp3', 'wb') as f:
-      f.write(response.content)
+        with open('podcast.mp3', 'wb') as f:
+            f.write(response.content)
 
-print('loading audio')
-audio = AudioSegment.from_mp3('podcast.mp3')
-# OR
-# audio = AudioSegment.from_file('session.mkv', 'aac')
-transcript = ''
+def load_audio(file_path):
+    print('Loading audio')
+    audio = AudioSegment.from_mp3(file_path)
+    return audio
 
-# Chunk duration in milliseconds
-chunk_duration_ms = 5 * 60 * 1000
-audio_length_ms = len(audio)
-num_chunks = int(audio_length_ms / chunk_duration_ms)
+def transcribe_audio(audio, model, prompt):
+    transcript = ''
+    chunk_duration_ms = 5 * 60 * 1000
+    audio_length_ms = len(audio)
+    num_chunks = int(audio_length_ms / chunk_duration_ms)
 
-for i in range(num_chunks):
-    start_time = i * chunk_duration_ms
-    end_time = min((i + 1) * chunk_duration_ms, audio_length_ms)
-    chunk = audio[start_time:end_time]
+    for i in range(num_chunks):
+        start_time = i * chunk_duration_ms
+        end_time = min((i + 1) * chunk_duration_ms, audio_length_ms)
+        chunk = audio[start_time:end_time]
 
-    # write the chunk to a temp file
-    with tempfile.NamedTemporaryFile(suffix='.mp3', delete=False) as f:
-        chunk.export(f.name, format='mp3')
-        temp_file_path = f.name
-        audio_file = open(temp_file_path, "rb")
+        with tempfile.NamedTemporaryFile(suffix='.mp3', delete=False) as f:
+            chunk.export(f.name, format='mp3')
+            temp_file_path = f.name
+            audio_file = open(temp_file_path, "rb")
 
-    # use the temp file in transcription
-    print('transcribing chunk', i)
-    transcript += openai.Audio.transcribe(
-        file=audio_file,
-        model="whisper-1",
-        # add the previous transcript as context for better results
-        prompt=transcript
-    )['text']
+        print('Transcribing chunk', i)
+        transcript += openai.Audio.transcribe(
+            file=audio_file,
+            model=model,
+            prompt=transcript
+        )['text']
 
-    os.remove(temp_file_path)
+        os.remove(temp_file_path)
 
-with open('transcript.txt', 'w') as f:
-    f.write(transcript)
+    return transcript
+
+def save_transcript(transcript, file_path):
+    with open(file_path, 'w') as f:
+        f.write(transcript)
+
+def main():
+    load_dotenv()
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    podcast_url = ''
+    download_podcast(podcast_url)
+
+    audio = load_audio('podcast.mp3')
+
+    model = "whisper-1"
+    prompt = ''
+    transcript = transcribe_audio(audio, model, prompt)
+
+    save_transcript(transcript, 'transcript.txt')
+
+if __name__ == "__main__":
+    main()
